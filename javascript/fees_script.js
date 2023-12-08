a09ff491f78d7a333c6cd05b5430f310a9293a1b
syedmurshed55 <91567003+syedmurshed55@users.noreply.github.com>
2023-12-08 23:07:54 +0600

    Add files via upload

---
+++ javascript/fees_script.js
@@ -0,0 +1,190 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1;
+
+    if(inputVal==476619){
+        document.getElementById("demo").innerHTML ="Syed Morsad ALi";
+        document.getElementById("demo1").innerHTML ="10000";
+    }
+    else if(inputVal==476620)
+    {
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";  
+    }
+    else if(inputVal==476621)
+    {
+        document.getElementById("demo").innerHTML ="Sajeeb";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==476622)
+    {
+        document.getElementById("demo").innerHTML ="Arman";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==476623)
+    {
+        document.getElementById("demo").innerHTML ="Sifath";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==476624)
+    {
+        document.getElementById("demo").innerHTML ="Rafi";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==476625)
+    {
+        document.getElementById("demo").innerHTML ="Mustafiz";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==476626)
+    {
+        document.getElementById("demo").innerHTML ="Mifta";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+    else if(inputVal==476628)
+    {
+        document.getElementById("demo").innerHTML ="Ruhan";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==476629)
+    {
+        document.getElementById("demo").innerHTML ="Rezaul";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }
+
+     else if(inputVal==476629)
+    {
+        document.getElementById("demo").innerHTML ="Oli";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+     else if(inputVal==476630)
+    {
+        document.getElementById("demo").innerHTML ="Shakil";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==476631)
+    {
+        document.getElementById("demo").innerHTML ="Rukon";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }  
+    else if(inputVal==476632)
+    {
+        document.getElementById("demo").innerHTML ="Arafath";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==476633)
+    {
+        document.getElementById("demo").innerHTML ="Sakib";
+        document.getElementById("demo1").innerHTML ="2500";   
+    }
+    else if(inputVal==476634)
+    {
+        document.getElementById("demo").innerHTML ="Adnan";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==476635)
+    {
+        document.getElementById("demo").innerHTML ="Afsar";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==476636)
+    {
+        document.getElementById("demo").innerHTML ="Adrian";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==476637)
+    {
+        document.getElementById("demo").innerHTML ="Fahim";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==476638)
+    {
+        document.getElementById("demo").innerHTML ="Sayed";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+     else if(inputVal==476638)
+    {
+        document.getElementById("demo").innerHTML ="Nadia";
+        document.getElementById("demo1").innerHTML ="1000"; 
+    }
+    else if(inputVal==476640)
+    {
+        document.getElementById("demo").innerHTML ="Khalil";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==47662941)
+    {
+        document.getElementById("demo").innerHTML ="Nabil";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==476645)
+    {
+        document.getElementById("demo").innerHTML ="Mahi";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==476646)
+    {
+        document.getElementById("demo").innerHTML ="Sultan";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==476647)
+    {
+        document.getElementById("demo").innerHTML ="Durjoy";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+     else if(inputVal==47662948)
+    {
+        document.getElementById("demo").innerHTML ="Atif";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==47662949)
+    {
+        document.getElementById("demo").innerHTML ="Samad";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+
+    else if(inputVal==476650)
+    {
+        document.getElementById("demo").innerHTML ="Ripi";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+     else if(inputVal==47662951)
+    {
+        document.getElementById("demo").innerHTML ="Mumina";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==47662952)
+    {
+        document.getElementById("demo").innerHTML ="Thania";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+    else if(inputVal==4766553)
+    {
+        document.getElementById("demo").innerHTML ="Muaz";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==476655)
+    {
+        document.getElementById("demo").innerHTML ="Enan";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==476656)
+    {
+        document.getElementById("demo").innerHTML ="Tayef";
+        document.getElementById("demo1").innerHTML ="20000 + 2500 = 22500"; 
+    }
+    else if(inputVal==476657)
+    {
+        document.getElementById("demo").innerHTML ="Tanjir";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else{
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";
+    }
+    
+}
+
